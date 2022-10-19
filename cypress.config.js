fe6f9bc54a5191a2029b1e88cc096031cc54dcaf
Iliandr123 <isapronov@tourmalinecore.com>
2022-10-19 16:39:28 +0500

    feat(allure): add allure reports and scripts for it

--- cypress.config.js
+++ cypress.config.js
@@ -3,13 +3,24 @@ const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 
 module.exports = defineConfig({
 
-  video: false,
-  screenshotOnRunFailure: false,
-  retries: {
-    runMode: 1,
-    openMode: 0,
-  },
   e2e: {
+    reporter: 'cypress-qase-reporter',
+    reporterOptions: {
+      apiToken: '0d34be7b8ca289973f1f955b5303c1f49b7cb844',
+      projectCode: 'INNCIRCLE',
+      runId: 1,
+      logging: true,
+    },
+    viewportWidth: 1200,
+    viewportHeight: 660,
+    specPattern: 'cypress/e2e/**/*.cy.js',
+    baseUrl: 'http://localhost:3000',
+    video: false,
+    screenshotOnRunFailure: false,
+    retries: {
+      runMode: 1,
+      openMode: 0,
+    },
     setupNodeEvents(on, config) {
       allureWriter(on, config);
       return config;
