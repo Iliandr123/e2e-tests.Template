3a010c657f7886ddebf7d1df659b3aaaf92c12b4
Iliandr123 <isapronov@tourmalinecore.com>
2022-10-27 10:22:44 +0500

    ref(helper): change end_time attr on status_text

--- helpers/qase-pre-script.js
+++ helpers/qase-pre-script.js
@@ -3,7 +3,7 @@ const QaseApiHelper = require('./QaseApiHelper');
 async function deleteLastTestRun() {
   const response = await QaseApiHelper.getAllTestRuns()
     .then((res) => res.result.entities);
-  const idsToDelete = response.filter(({ end_time: endTime }) => endTime === null);
+  const idsToDelete = response.filter(({ status_text: statusText }) => statusText === 'active');
 
   if (idsToDelete.length === 2) {
     QaseApiHelper.deleteTestRun(idsToDelete[0].id);
